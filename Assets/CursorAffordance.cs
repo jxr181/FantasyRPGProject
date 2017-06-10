080e370fe7388c0e468cbff40d6de4b5d2d59a24
DESKTOP-KJ7HHE8\jeoma <jeomarinteractive@gmail.com>
2017-06-10 15:40:05 -0400

    Section 1 - Lecture 15 - CursorAffordance

--- Assets/CursorAffordance.cs
+++ Assets/CursorAffordance.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CursorAffordance : MonoBehaviour
 {
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Vector2 cursorHotspot = new Vector2(96, 96);
+    [SerializeField] Texture2D enemyCursor = null;
+    [SerializeField] Texture2D UnknownCursor = null;
 
     CameraRaycaster cameraRaycaster;
 
@@ -16,6 +20,23 @@ public class CursorAffordance : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        //print(cameraRaycaster.layerHit);
+        switch (cameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+
+            case Layer.Enemy:
+                Cursor.SetCursor(enemyCursor, cursorHotspot, CursorMode.Auto);
+                break;
+
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(UnknownCursor, cursorHotspot, CursorMode.Auto);
+                break;
+
+            default:
+                Debug.LogError("Don't know what cursor to show");
+                return;
+        }
 	}
 }
