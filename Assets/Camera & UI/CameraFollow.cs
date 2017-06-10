f48d443c795eae68524b5a4c48fd3ab9882466bd
DESKTOP-KJ7HHE8\jeoma <jeomarinteractive@gmail.com>
2017-06-10 13:04:30 -0400

    Section 1 - Lecture 10 CameraRaycaster

---
+++ Assets/Camera & UI/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Variables
+    GameObject player;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        print(player.ToString());
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        transform.position = player.transform.position;
+	}
+}
