2a22f4e22b69f33e43b54d1b64ed4f3d868702b3
DESKTOP-KJ7HHE8\jeoma <jeomarinteractive@gmail.com>
2017-06-11 22:19:31 -0400

    Section 2 - Lecture 43 - Make Enemies Path Find

--- Assets/Characters/Enemies/Enemy.cs
+++ Assets/Characters/Enemies/Enemy.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float attackRadius = 4f;
+    [SerializeField] float chaseRadius = 4f;
 
     float currentHealthPoints = 100f;
+    AICharacterControl aiCharacterControl = null;
+    GameObject player =null;
 
     public float healthAsPercentage
     {
@@ -14,5 +17,44 @@ public class Enemy : MonoBehaviour
         {
             return currentHealthPoints / (float)maxHealthPoints;
         }
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+
+        if (distanceToPlayer <= attackRadius)
+        {
+            print("Attacking Player!");
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
+        }
+
+        if (distanceToPlayer <= chaseRadius)
+        {
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw Move and Attack Gizmos
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, chaseRadius);
     }
 }
